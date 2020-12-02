3ee598680d7238c7a2f6c605426b70031a3c1772
超人不会飞 <977735103@qq.com>
2020-12-02 08:53:43 +0800

    5.0

---
+++ src/components/Student.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+       <p>名字:{{name}}</p>
+       <p>年纪：{{age}}</p>
+
+    </div>
+</template>
+<script>
+    import {inject, toRefs} from "vue"
+    export default {
+        setup(){
+            const student = inject('student')
+            const dataStudent = toRefs(student)
+            return {...dataStudent}
+        }
+    }
+</script>
--- src/views/Ceshi.vue
+++ src/views/Ceshi.vue
@@ -1,5 +1,22 @@
 <template>
     <div>
-        测试
+       <Student></Student>
     </div>
 </template>
+<script>
+    import Student from "../components/Student.vue"
+    import {provide,reactive} from "vue"
+    export default {
+        components:{
+           Student 
+        },
+        setup(){
+            const student = reactive({
+                name:'刘超峰',
+                age:28
+            })
+            provide('student',student)
+          
+        }
+    }
+</script>
